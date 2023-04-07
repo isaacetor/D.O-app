41561ac030c4a058b2b601f6b4b9431dd805fc8e
Ajinung <isaacetor7@gmail.com>
2023-04-07 02:45:56 +0100

    done with client and agent auth with responsiveness

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,15 @@
+//types for managing props state for user and agent auth sidebar
 export type authPropsData = {
   title: string;
   desc: string;
   backgroundColor: string;
   CardColor: string;
 };
+
+//types for managing global buttons
+export type GlobalButtonProp = {};
+
+//types for managing user details
 export type userData = {
   name: string;
 };
