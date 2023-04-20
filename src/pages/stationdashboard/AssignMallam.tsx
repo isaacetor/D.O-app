6013218303b58fd7dc2c7ccf07232ca542639386
Phinatech <ugbojudithchinemerem@gmail.com>
2023-04-20 23:27:46 +0100

    assign mallam

--- src/pages/stationdashboard/AssignMallam.tsx
+++ src/pages/stationdashboard/AssignMallam.tsx
@@ -3,12 +3,42 @@ import styled from "styled-components";
 const AssignMallam = () => {
   return (
     <Container>
-      <Wrapper></Wrapper>
+      <Wrapper>
+        <Title>
+          <Div>Hello</Div>
+          <Div>Hello</Div>
+          <Div>Hello</Div>
+          <Div>Hello</Div>
+          <Div>Hello</Div>
+          <Div>Hello</Div>
+        </Title>
+        <Status1>
+          <Div>23 Jan, 2023.</Div>
+          <Div>x42356</Div>
+          <Div>10.00pm</Div>
+          <Div>Open</Div>
+          <Div>11.00pm</Div>
+        </Status1>
+      </Wrapper>
     </Container>
   );
 };
 
 export default AssignMallam;
+const Div = styled.div``;
+const Status1 = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2fr 2fr 2fr 2fr 2fr 2fr;
+  padding-top: 10px;
+
+  border-top: 1px solid grey;
+`;
+const Title = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2fr 2fr 2fr 2fr 2fr 2fr;
+`;
 const Container = styled.div`
   width: calc(100% - 250px);
   display: flex;
@@ -17,5 +47,4 @@ const Container = styled.div`
 `;
 const Wrapper = styled.div`
   width: 100%;
-  background-color: red;
 `;
