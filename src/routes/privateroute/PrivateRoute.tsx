0fe50dacc0efd06dfb66e4813ed3a4693b11526a
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-05-11 01:44:04 +0100

    working on pwa

--- src/routes/privateroute/PrivateRoute.tsx
+++ src/routes/privateroute/PrivateRoute.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+
 import { Navigate } from "react-router-dom";
 import { useAppSelector } from "../../services/statemanagement/Store";
 
 const PrivateRoutes = ({ children }: any) => {
   const user = useAppSelector((state) => state.userDetails);
+  const station = useAppSelector((state) => state.stationdetail)
+  const director =useAppSelector((state)=> state.directorDetails)
+
+
 
   const check = user?.name;
+  const stationUser = station?._id
+  const directorUSer = director?.name
 
   if (check) {
     return children;
-  } else {
+  } else if (stationUser) {
+    return children
+  } else if (directorUSer) {
+    return children
+  }
+  else {
     return <Navigate to="/" replace />;
   }
 };
